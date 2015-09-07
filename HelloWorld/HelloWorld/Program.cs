8d0e1aab04bb7f5ba47ed1adfaf5a05399220ab4
vuvid <vv@clienti.dk>
2015-09-08 00:04:53 +0200

    Revert "Removing call to Read()"
    This reverts commit 17f9a49bebe2409c634e3df4e855f3ff1372bae5.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
-           // Console. Read();
+            Console.Read();
         }
     }
 }
